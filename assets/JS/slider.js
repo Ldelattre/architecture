9b03f3895a087223d79ca39c437505f8e968ac78
leandre.delattre <leandre.delattre41@gmail.com>
2022-01-16 18:05:27 +0100

    first commit

---
+++ assets/JS/slider.js
@@ -0,0 +1,33 @@
+let slider = document.querySelector('.slider');
+if (slider) {
+    let slides = slider.querySelectorAll('.slide');
+    let nbSlide = slides.length;
+    let showSlide = 0;
+    for (let i = 0; i < slides.length; i++) {
+        let slide = slides[i];
+        if (i === 0) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    }
+    let prevArrow = slider.querySelector('.prev');
+    let nextArrow = slider.querySelector('.next');
+    prevArrow.addEventListener('click', function () {
+        slides[showSlide].style.display = 'none';
+        showSlide -= 1;
+        if (showSlide < 0) {
+            showSlide = nbSlide - 1;
+        }
+        slides[showSlide].style.display = 'block';
+    });
+
+    nextArrow.addEventListener('click', function () {
+        slides[showSlide].style.display = 'none';
+        showSlide += 1;
+        if (showSlide >= nbSlide) {
+            showSlide = 0;
+        }
+        slides[showSlide].style.display = 'block';
+    });
+}
