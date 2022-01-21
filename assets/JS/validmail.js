77b0296624c0f3572a1aeb1cd08db4fac083a114
leandre.delattre <leandre.delattre41@gmail.com>
2022-01-21 15:11:52 +0100

    maj vendredi midi

--- assets/JS/validmail.js
+++ assets/JS/validmail.js
@@ -2,11 +2,12 @@ function validateEmail(email) {
   let res = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return res.test(email);
 }
+
 function validate() {
   let result = $("#result");
   let email = $("#email").val();
   result.text("");
-  if(validateEmail(email)) {
+  if (validateEmail(email)) {
     result.text(email + " is valid");
     result.css("color", "blue");
   } else {
